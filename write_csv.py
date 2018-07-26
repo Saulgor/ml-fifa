0a9e8c3c7f1a82f4950aeb870e79b902e5c24891
paul.chen <paul.chen@scmp.com>
2018-07-26 17:56:08 +0800

    add write csv file

---
+++ write_csv.py
@@ -0,0 +1,6 @@
+import csv
+ 
+with open('persons.csv', 'wb') as csvfile:
+    filewriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    filewriter.writerow(['Name', 'Profession'])
